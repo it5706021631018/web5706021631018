fb567e6277b56f9d7715b718adb263bd56e1da54
it5706021631018 <5706021631018@fitm.kmutnb.ac.th>
2016-02-11 23:04:36 +0700

    oopครั้งที่1

---
+++ Trik.java
@@ -0,0 +1,22 @@
+/**
+ * Chutikan Kantawin
+ * IT-2RB
+ * 5706021631018
+*/
+<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+	<name>Trik</name>
+	<comment></comment>
+	<projects>
+	</projects>
+	<buildSpec>
+		<buildCommand>
+			<name>org.eclipse.jdt.core.javabuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+	</buildSpec>
+	<natures>
+		<nature>org.eclipse.jdt.core.javanature</nature>
+	</natures>
+</projectDescription>
