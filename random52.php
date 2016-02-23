e1c21c41c6239162fa4a1e48c79b3fab8356921a
it5706021631018 <5706021631018@fitm.kmutnb.ac.th>
2016-02-23 23:07:52 +0700

    Update random52.php

--- random52.php
+++ random52.php
@@ -2,27 +2,50 @@
 <head> <title> RANDOM52 </title> </head>
 <body background="m55.jpg">
 <?php
-echo "<table>"
 
-for($a==1;$a<4;$a++){
- echo "<tr>"
- echo "<td>"
-$number='1234567890';
-for($i==1;$i<13;$i++){
-$random=rand(0,strlen($number)-1);
-$cut_txt=substr($number,$random,1);
-$result.=substr($number,$random,1);
-$number=str_replace($cut_txt,'',$number);
+$row = 13;
+$col = 4;
+$cards = array();
+for ($i = 0; $i < 1; $i++) { 
+       	$card = mt_rand(1, 52); 
+       	if(!in_array($card, $cards)){
+       		$cards[$i] = $card;
+	}else{
+		$i--;
+	}
+	$cards = array_unique($cards);
+   	} 
+
+$count_arr=count($cards); 
+
+   	foreach ($cards as $cards) { 
+	
+	}
+echo "<table align='center' border='4' width='300'>";
+
+for ($r=1; $r <= $row; $r++){
+echo "<tr>";
+for ($c=1; $c <= $col; $c++){
+if ($r==$c){
+echo "<td align='center'>";
+echo $cards."</td>";
 }
-
-$i=0;
-echo $result."<br/>";
-$result='';
+else
+{
+echo "<td align='center'> $cards</td>";
 }
-echo "</tr>;
+if($cards>=52)
+{$cards = ($cards=1);
+}
+elseif ($cards==$cards)
+{$cards = ($cards+1);
+}
+}
+echo "</tr>";
+}
+echo "</table>";
 
 
-echo "</table>"
  ?>
 </body>
 </html>
